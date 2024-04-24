e0fb32f81c31a10858ea8a559f6226c5ee8c34c9
webtangen <143550134+webtangen@users.noreply.github.com>
2024-04-24 20:07:10 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,33 @@
+    const data = [];
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+
+    return data;
+    for (let i = 0; i < 10; i++) {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+function generateRandomData() {
+    }
+
+    const data = generateRandomData();
+}
+main();
+    });
+function main() {    }
+function generateRandomData() {
+    const data = [];
+    return data;
+
+function main() {
+
+main();
+    const data = generateRandomData();
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+    });
+    for (let i = 0; i < 10; i++) {
+}
+        console.log("Random Number: " + item);
